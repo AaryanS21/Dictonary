7d6c6616030e3523ae19b6ea69f86dbf8b6703e6
AaryanS21 <aaryan.sharma0411@gmail.com>
2024-12-02 20:02:02 +0000

    Add files via upload

---
+++ lesson 1/main.py
@@ -0,0 +1,14 @@
+#making a Dictionary
+#Dictornary is a collection of items in a key value pair similar to list but without index numbers
+
+person= {'name' : 'sanjana' , 'age' : 26 , 'hobby': 'Watching movies'} 
+print(person)
+
+#Acess the values using keys
+print(person['name']) 
+
+print(person['hobby']) 
+
+#adding new item to the dictionary
+person['country'] = 'England' #country is a key and England is a value for it
+print(person)
